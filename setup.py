abb5da6cc6de2c4f15f2f435cdbd2bde556b9784
Andreas Leitherer <leitherer@fhi-berlin.mpg.de>
2021-03-19 08:09:16 +0100

    Commented out Python 3.7 spec in setup.py

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ def main():
             # 'Programming Language :: Python :: 2.7',
             # 'Programming Language :: Python :: 3.5',
             # 'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7'
+            # 'Programming Language :: Python :: 3.7'
         ],
 
         # What does your project relate to?
